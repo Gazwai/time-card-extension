0aa283643f8839a59249b040e68fd6509dc0ab81
Gary Y <garyyau080@gmail.com>
2023-02-23 20:31:40 +0900

    refactor: bug fix

--- Mr_Wolf/scripts/index.js
+++ Mr_Wolf/scripts/index.js
@@ -1,16 +1,20 @@
-// Have an input for default start time
-// Have an input for weekly hours
-
 window.onload = (event) => {
+  elem.addEventListener("click", (event) => {
+    event.preventDefault();
+    setEmail_Apitoken();
+    authFunction();
+    sendAuth_userMsg_background();
+  });
+
   const today = new Date().getDay();
 
   const startAndEndTime = document
-  .querySelectorAll("div.cal_day div.inner")
-  [today]?.innerText.match(/\d\d.\d\d/gi)
+    .querySelectorAll("div.cal_day div.inner")
+    [today]?.innerText.match(/\d\d.\d\d/gi);
 
   //Works out time remaining for the week
   var timeLeftThisWeek = timeLeft(40);
-  var weeklyHoursDividedByDays = timeLeftThisWeek / (daysLeftThisWeek(today));
+  var weeklyHoursDividedByDays = timeLeftThisWeek / daysLeftThisWeek(today);
 
   function daysLeftThisWeek(today) {
     if (startAndEndTime?.length == 2) {
@@ -28,8 +32,8 @@ window.onload = (event) => {
   var breakDuration = 1;
 
   var breakArray = document
-  .querySelectorAll(".rest.text-middle")
-  [today - 1]?.innerText.match(/\d\d.\d\d/g);
+    .querySelectorAll(".rest.text-middle")
+    [today - 1]?.innerText.match(/\d\d.\d\d/g);
 
   if (breakArray != null && breakArray.length > 2) {
     var breakArray = [breakArray.slice(0, 2), breakArray.slice(2)];
@@ -44,7 +48,9 @@ window.onload = (event) => {
   var endTimeInString = endTime();
 
   const newDiv = document.createElement("span");
-  newDiv.innerHTML = `Hours left: ${timeLeftThisWeek}/${weeklyHoursDividedByDays.toFixed(2)} : Ending Time: ${endTimeInString}pm`;
+  newDiv.innerHTML = `Hours left: ${timeLeftThisWeek}/${weeklyHoursDividedByDays.toFixed(
+    2
+  )} : Ending Time: ${endTimeInString}pm`;
   newDiv.style.margin = "1rem";
 
   document
@@ -63,11 +69,10 @@ window.onload = (event) => {
   function todayHrAndMin() {
     // We then get the time worked today and subtract it from the 8 hours we work a day plus a 1 hour lunch break.
     if (startAndEndTime?.length == 2) {
-      return 8
+      return 8;
     }
 
-    let start = startAndEndTime?.[0]
-      .split(":");
+    let start = startAndEndTime?.[0].split(":");
 
     return start ? parseFloat(start[0]) + parseFloat(start[1] / 60) : 8;
   }
@@ -75,11 +80,11 @@ window.onload = (event) => {
   function calculateBreak(time) {
     var todayBreakStart = time[0].split(":");
     var startBreakStartInFloat =
-    parseFloat(todayBreakStart[0]) + parseFloat(todayBreakStart[1] / 60);
+      parseFloat(todayBreakStart[0]) + parseFloat(todayBreakStart[1] / 60);
 
     var todayBreakEnd = time[1].split(":");
     var startBreakEndInFloat =
-    parseFloat(todayBreakEnd[0]) + parseFloat(todayBreakEnd[1] / 60);
+      parseFloat(todayBreakEnd[0]) + parseFloat(todayBreakEnd[1] / 60);
 
     breakDuration += parseFloat(
       (startBreakEndInFloat - startBreakStartInFloat).toFixed(2)
@@ -88,14 +93,14 @@ window.onload = (event) => {
 
   function endTime() {
     var endTimeInFloat =
-    weeklyHoursDividedByDays + breakDuration + startTimeInFloat;
+      weeklyHoursDividedByDays + breakDuration + startTimeInFloat;
 
     var endTimeInArray = endTimeInFloat.toFixed(2).split(".");
 
     return typeof endTimeInArray === "string"
       ? "Day Off 🚀"
-      : `${endTimeInArray[0]}:${singleDigits(Math.round(
-          endTimeInArray[1] * 0.6)
+      : `${endTimeInArray[0]}:${singleDigits(
+          Math.round(endTimeInArray[1] * 0.6)
         )}`;
   }
 
@@ -104,5 +109,34 @@ window.onload = (event) => {
       return "0" + params;
     }
     return params;
+  }
+
+  // ---------------------------------------------
+
+  // function to get user input and save it in local storage
+  function setDefaultValues() {
+    window.localStorage.setItem(
+      "hours",
+      document.getElementById("form_email").value
+    );
+    window.localStorage.setItem(
+      "start-time",
+      document.getElementById("form_api_token").value
+    );
+    document.getElementById("hours").value = "";
+    document.getElementById("start-time").value = "";
+  }
+
+  // Wrote a function to send stored email & api token to the service worker(listens to browser action)
+  function sendAuth_userMsg_background() {
+    let pop_u = window.localStorage.getItem("user");
+    let pop_t = window.localStorage.getItem("api_token");
+    let pop_c = window.localStorage.getItem("child_user");
+    chrome.runtime.sendMessage({
+      name: pop_u,
+      token: pop_t,
+      child: pop_c,
+      action: "pop_auth",
+    });
   }
 };

90e92d8de1f06bcf51969500912bde8de8e6624f
Gary Y <garyyau080@gmail.com>
2023-02-24 19:20:43 +0900

    refactor: refactor background code

--- Mr_Wolf/background.js
+++ Mr_Wolf/background.js
@@ -1,41 +1,42 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+// Listen for a message to set default values in storage
+chrome.runtime.onMessage.addListener(setDefaultValues);
+
+// Listen for a message to send default values to the active tab
+chrome.runtime.onMessage.addListener(sendDefaultValuesIfOnSmaregi);
+
+function setDefaultValues(request, sender, sendResponse) {
   if (request.action === "default_values") {
-    let service_worker_hours = request.hours;
-    let service_worker_start = request.start;
-    chrome.storage.sync.set({
-      hours: service_worker_hours,
-      start: service_worker_start,
+    const { hours, start } = request;
+    chrome.storage.sync.set({ hours, start }, () => {
+      console.log("Setting values");
     });
-    console.log("setting values");
     return true;
   }
-});
+}
 
-// Wrote a function to send stored default values to the active tab if the url has smaregi timecard, checks every 1 seconds
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+function sendDefaultValuesIfOnSmaregi(request, sender, sendResponse) {
   if (request.action === "on_smaregi") {
-    console.log("connected");
+    console.log("Connected");
     sendDefaultValues();
+    return true;
   }
-  return true;
-});
+}
 
 function sendDefaultValues() {
-  chrome.storage.sync.get(["hours", "start"], function (data) {
-    let back_hours = data.hours;
-    let back_start = data.start;
-
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      if (
-        tabs[0].url?.match("https://timecard1.smaregi.jp/staffs/dashboard/*")
-      ) {
-        chrome.tabs.sendMessage(tabs[0]?.id, {
-          hours: back_hours,
-          start: back_start,
+  chrome.storage.sync.get(["hours", "start"], ({ hours, start }) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const tab = tabs.find((tab) =>
+        tab.url?.match("https://timecard1.smaregi.jp/staffs/dashboard/*")
+      );
+      if (tab) {
+        const { id } = tab;
+        chrome.tabs.sendMessage(id, {
+          hours,
+          start,
           action: "back_default_values",
         });
       }
     });
-    console.log("getting values");
+    console.log("Getting values");
   });
 }

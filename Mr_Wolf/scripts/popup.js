53351bf81403e72dd0700f8c07f83af93d6a9a93
Gary Y <garyyau080@gmail.com>
2023-02-24 19:09:33 +0900

    refactor: refactor code

--- Mr_Wolf/scripts/popup.js
+++ Mr_Wolf/scripts/popup.js
@@ -1,30 +1,25 @@
 window.onload = () => {
-  const hours = document.querySelector("#hours");
-  const start = document.querySelector("#start-time");
+  const hoursInput = document.querySelector("#hours");
+  const startInput = document.querySelector("#start-time");
 
-  hours.value = window.localStorage.getItem("hours") || 40;
-  start.value = window.localStorage.getItem("start") || 8;
+  hoursInput.value = localStorage.getItem("hours") || 40;
+  startInput.value = localStorage.getItem("start") || 8;
 
-  document.querySelector("#submit").addEventListener("click", (event) => {
+  const submitButton = document.querySelector("#submit");
+  submitButton.addEventListener("click", (event) => {
     event.preventDefault();
-    setDefaultValues();
+    saveDefaultValues();
     sendValuesToBackground();
   });
 
-  // function to get user input and save it in local storage
-  function setDefaultValues() {
-    window.localStorage.setItem("hours", hours.value);
-    window.localStorage.setItem("start", start.value);
+  function saveDefaultValues() {
+    localStorage.setItem("hours", hoursInput.value);
+    localStorage.setItem("start", startInput.value);
   }
 
-  // Wrote a function to send stored default values to the service worker(listens to browser action)
   function sendValuesToBackground() {
-    let hour = window.localStorage.getItem("hours");
-    let start = window.localStorage.getItem("start");
-    chrome.runtime.sendMessage({
-      hours: hour,
-      start: start,
-      action: "default_values",
-    });
+    const hours = localStorage.getItem("hours");
+    const start = localStorage.getItem("start");
+    chrome.runtime.sendMessage({ hours, start, action: "default_values" });
   }
 };
